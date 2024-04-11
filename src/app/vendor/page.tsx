fc44a663b54a3a71d627b82b795a7e0ff58094fb
Maratib Ali Khan <maratib@gmail.com>
2024-04-12 00:21:22 +0500

    Shadcn added

---
+++ src/app/vendor/page.tsx
@@ -0,0 +1,18 @@
+// import { UserDao } from "@/lib/dao/userDao";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default async function Home() {
+  let users = null;
+
+  // try {
+  //   users = await new UserDao().getUsers();
+  // } catch (error) {}
+
+  return (
+    <main>
+      <h1 className="text-red-600">Hello</h1>
+      <Button>Click me</Button>
+    </main>
+  );
+}
